b384d57e1943a9bf11a6f64588bd950281df4a87
Alexander Morozov <frostmoroz83@yandex.ru>
2019-06-27 22:05:05 +0400

    начал работать с cookie

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -20,14 +20,27 @@ const useStyles = makeStyles(theme => ({
         height: 450,
     },
 }));
+// возвращает cookie с именем name, если есть, если нет, то undefined
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 
 class Content extends Component {
     componentWillMount() {
-        console.log(`Это пропсы `, this.props);
+
         const { setPoster } = this.props;
+
         axios.get('/db.json').then(({data}) => {
-            setPoster(data);
-        })
+            const date = new Date(new Date(Date.now() + 7 * 86400000).toGMTString());
+            document.cookie = "db=" + JSON.stringify(data) + "; path=/; expires=" + date.toUTCString();
+            let cook = JSON.parse(getCookie('db'));
+            setPoster(cook);
+            console.log(cook);
+        });
     }
 
     render() {
